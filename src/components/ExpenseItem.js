3a49a7ba763fdf2f1f5b18ee648b04165f2889ee
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-15 15:30:54 -0300

    Adding Basic CSS Styling
    In this commit, the file containing the ExpenseItem component's css was created in the same folder with the same name, just differentiating it with the .css ending. The ExpenseItem.css file was injected inside the ExpenseItem.js component. To name a class in html within js you must use className and not just class as in normal html.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2>Expense item!</h2>;
+    return (
+        <div className="expense-item">
+            <div>March 15th 2023</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
