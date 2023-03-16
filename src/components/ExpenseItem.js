1dab2f8bb49535b62c3e4fd5461fcb26f199650f
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-16 10:23:22 -0300

    Outputting Dynamic Data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>March 15th 2023</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
