f89b175fbe9b5995145600ac2f00ed844382a7a6
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-17 09:25:39 -0300

    Adding Form Submission to Store Data
    In this commit Form submission was set to store the data and after immediately clear the input fields

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,56 +5,50 @@ const ExpenseForm = () => {
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
     const [enteredDate, setEnteredDate] = useState('');
-    // const [userInput, setUserInput] = useState({
-    //     enteredTitle: '',
-    //     enteredAmount: '',
-    //     enteredDate: ''
-    // });
 
     const titleChangeHandler = event => {
         setEnteredTitle(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredTitle: event.target.value
-        // });
-        // // setUserInput(prevState => {
-        // //     return { ...prevState, enteredTitle: event.target.value };
-        // // });
-        console.log(event.target.value);
+        // console.log(event.target.value);
     };
 
     const amountChangeHandler = event => {
         setEnteredAmount(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredAmount: event.target.value
-        // });
-        console.log(event.target.value);
+        // console.log(event.target.value);
     };
 
     const dateChangeHandler = event => {
         setEnteredDate(event.target.value);
-        // setUserInput({
-        //     ...userInput,
-        //     enteredDate: event.target.value
-        // });
-        console.log(event.target.value);
+        // console.log(event.target.value);
+    };
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amout: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2022-01-01" max="2024-12-28" onChange={dateChangeHandler} />
+                    <input type="date" min="2022-01-01" max="2024-12-28" value={enteredDate} onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className="new-expense__actions">
