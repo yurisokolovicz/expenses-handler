75abebafcbda71db819d2ce0731c0fe833fb1e16
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-17 10:05:03 -0300

    Child-to-Parent Component Communication another example
    In this commit a props called onAddExpense was created in the App.js to allow communication with the NewExpense.js. In the NewExpense.js a props was passed in the NewExpense as parameter and the props was called in the end of the NewExpense function code passing the stored expenseData information to the App.js. To prove the data is coming from App.js a console.log was created in line 30 and that is the one that appears in the console.

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
-const NewExpense = () => {
+const NewExpense = props => {
     const SaveExpenseDataHandler = enteredExpenseData => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString() // to create a random number as id of the event.
         };
-        console.log(expenseData);
+        props.onAddExpense(expenseData);
     };
 
     return (
